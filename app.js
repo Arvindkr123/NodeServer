a5881f3ffd97994e24ca8b8b350d55de89839364
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-05 21:02:15 +0530

    send data to the server

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  // console.log("url of request ->>>>", req.url);
+  // console.log("methods of request ->>>>> ", req.method);
+  // console.log("headers of request ->>>>>>", req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write(`<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+    <body>
+      <h1>Hello From my Node js Server</h1>
+    </body>
+  </html>
+  `);
+  res.end();
 });
 
 server.listen(4000);
