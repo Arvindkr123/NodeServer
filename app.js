a3708516bc6a80f2720bdf90775815c9a9a941cb
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-19 17:29:37 +0530

    added routing request

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write(`
+    <html>
+      <head>
+         <title>Enter Message</title>
+      </head>
+      <body>
+        <form action="/message" method="POST">
+           <input type="text" name="message" />
+           <button type="sumit">send</button>
+        </form>
+      </body>
+    </html>
+    `);
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head>");
