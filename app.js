b5aa205a1481a13c104add62bbdc26885b7e8ff3
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-19 21:47:33 +0530

    refactor code

--- app.js
+++ app.js
@@ -1,62 +1,6 @@
 const http = require("http");
-const fs = require("fs");
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    return fs.readFile("message.txt", "utf8", (err, data) => {
-      console.log(data);
-      res.write(`
-      <html>
-      <head>
-      <title>Enter Message</title>
-      </head>
-      <body>
-        <form action="/message" method="POST">
-        ${data}
-           <input type="text" name="message" />
-           <button type="sumit">send</button>
-           </form>
-           </body>
-           </html>
-           `);
-      return res.end();
-    });
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      // console.log(chunk);
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parseBody = Buffer.concat(body).toString();
-      // console.log(parseBody);
-      const message = parseBody.split("=")[1];
-      fs.writeFile("message.txt", message, () => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head>");
-  res.write("<title>");
-  res.write("Welcome to first page!");
-  res.write("</title>");
-  res.write("</head>");
-  res.write(`
-          <body>
-            <h1>Welcome to first Node.js page!!</h1>
-          </body>
-  `);
-  res.write("</html>");
-  res.end();
-});
+const routes = require("./routes");
+const server = http.createServer(routes);
 
 server.listen(3001, () => {
   console.log("listening on port 3001...");
