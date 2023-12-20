01f662f7aece3d494a9efa7ea3dede32312f0452
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-20 11:32:57 +0530

    added

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
 app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page not found!!</h1>");
 });
--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/addProduct", (req, res, next) => {
+router.get("/add-product", (req, res, next) => {
   console.log("in the another middleware");
   res.send(
-    "<form action='/product' method='POST'><input type='text' placeholder='add product' name='title'/><input type='text' placeholder='enter size of product' name='size'/><button type='submit'>Add Product</button></form>"
+    "<form action='/admin/add-product' method='POST'><input type='text' placeholder='add product' name='title'/><input type='text' placeholder='enter size of product' name='size'/><button type='submit'>Add Product</button></form>"
   );
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
