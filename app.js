d970827d255f5251e10bd15012b7356f93bbf13f
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-20 09:26:30 +0530

    added middle ware

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const http = require("http");
-const routes = require("./routes");
-const server = http.createServer(routes);
+const express = require("express");
+const app = express();
 
-server.listen(3001, () => {
-  console.log("listening on port 3001...");
+app.use((req, res, next) => {
+  console.log("in the middleware");
+  next(); // allow the request to continue the other middleware with help of next
 });
 
+app.use((req, res, next) => {
+  console.log("in the another middleware");
+  res.send("<h1>Welcome to ExpressJS!!</h1>");
+});
+
+const server = http.createServer(app);
+server.listen(3001);
